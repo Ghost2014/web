8222287317d8438a0ed1ae936dfd7cea1fb22eb9
wpk <wpk@gmail.com>
2015-06-25 07:45:35 +0800

    dfsaf

--- myWeb/index.php
+++ myWeb/index.php
@@ -9,7 +9,8 @@
 
 //配置公用路径
 define('APP_SITE','localhost');
-define('APP_URL','myWeb/index.php/home/');
+define('APP_URL','/myWeb/index.php/home');
+define('APP_IMG','/myWeb/static/img');
 
 //开启调试模式
 define('APP_DEBUG',TRUE);
